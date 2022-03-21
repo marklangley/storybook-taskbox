b293f87f9e0951b6330e21d13dc46e36aa09847a
Mark Langley <langley.mark@gmail.com>
2022-03-20 21:44:01 -0400

    Finish 'Composite component' chapter

--- src/components/Task.js
+++ src/components/Task.js
@@ -14,7 +14,7 @@ export default function Task({ task: {id, title, state}, onArchiveTask, onPinTas
               <input type="text" value={title} readOnly={true} placeholder="Input title" />
           </div>
 
-          <div className="actions" onClick={event => event.stopImmediatePropagation()}>
+          <div className="actions" onClick={event => event.stopPropagation()}>
               {state !== 'TASK_ARCHIVED' && (
                   //eslint-disable-next-line jsx-a11y/anchor-is-valid
                   <a onClick={() => onPinTask(id)}>
