cfe43d548e645bac53e5c41229ae5b25a641e01f
Mark Langley <langley.mark@gmail.com>
2022-03-23 21:36:50 -0400

    Test edit to trigger workflow

--- src/components/InboxScreen.js
+++ src/components/InboxScreen.js
@@ -17,7 +17,7 @@ export default function InboxScreen() {
               <div className="wrapper-message">
                   <span className="icon-face-sad"/>
                   <div className="title-message">Oh no!</div>
-                  <div className="subtitle-message">Something went wrong</div>
+                  <div className="subtitle-message">Something went wrong!!</div>
               </div>
           </div>
         );
